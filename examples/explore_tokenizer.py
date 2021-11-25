36116b95d9d39b9e67903d07cb507384dc94799c
hale-in <78592027+hale-in@users.noreply.github.com>
2021-11-25 21:20:01 +0900

    [#7] include dataset dir

--- examples/explore_tokenizer.py
+++ examples/explore_tokenizer.py
@@ -3,9 +3,10 @@ from transformers import BertTokenizer, AutoTokenizer
 
 
 def main():
-    sent = "모순아니냐는 비판이 일각에서 나왔다"
+    sent = "중국 베이징에서 근무했던 한 외교 소식통은 \"중국에서는 관료나 기업인, 유명인사 누구든지 쥐도 새도 모르게 당국에 끌려갈 수 있다\"며 \"중국 공산당이 통치를 유지하기 위한 공포정치의 한 수단\"이라고 귀띔했다."
     tokenizer = BertTokenizer.from_pretrained("kykim/bert-kor-base")
     encoded = tokenizer(sent)
+    print(encoded)
     print(encoded['input_ids'])
     print([tokenizer.decode(token_id) for token_id in encoded['input_ids']])
 
